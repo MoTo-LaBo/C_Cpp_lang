ac990fac7f42b9ec3ee2f1bab9fa90c4071100a4
MoTo <moto.labo.desgin@gmail.com>
2021-09-15 10:40:33 +0900

    calculation add

---
+++ C_lang/01_演算/calculation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+    演算子を用いた計算プログラム
+*/
+
+void main()
+{
+    // 各種演算子
+    printf("%d + %d = %d\n", 5, 2, 5 + 3);
+    printf("%d - %d = %d\n", 5, 2, 5 - 2);
+    printf("%d * %d = %d\n", 5, 2, 5 * 2);
+    printf("%d / %d = %d 余り %d\n", 5, 2, 5 / 2, 5 % 2);
+}
